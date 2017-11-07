4a3139a5e3f1d34b0d67b68b96af793e5d109397
hailee <haitho0710@gmail.com>
2017-11-08 00:18:06 +0700

    edit youtube

--- app/src/main/java/com/example/hailee/clonechatvl/PostDetail.kt
+++ app/src/main/java/com/example/hailee/clonechatvl/PostDetail.kt
@@ -6,11 +6,18 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.jaedongchicken.ytplayer.JLog
+import com.jaedongchicken.ytplayer.YoutubePlayerView
+import com.jaedongchicken.ytplayer.model.YTParams
 import com.pierfrancescosoffritti.youtubeplayer.player.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.youtubeplayer.player.YouTubePlayer
 import com.pierfrancescosoffritti.youtubeplayer.player.YouTubePlayerInitListener
 import com.pierfrancescosoffritti.youtubeplayer.player.YouTubePlayerView
 import kotlinx.android.synthetic.main.fragment_post_detail.*
+import android.R.attr.duration
+import android.content.Context
+import android.support.v4.app.FragmentActivity
+import android.widget.Toast
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -24,21 +31,23 @@ private const val ARG_PARAM2 = "param2"
  */
 class PostDetail : Fragment() {
 
-    var youtubePlayer : YouTubePlayer? = null
+    private var youTubePlayer: YouTubePlayer? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        youtube_player_view.initialize(object : YouTubePlayerInitListener {
-            override fun onInitSuccess(initializedYouTubePlayer : YouTubePlayer?) {
-                youtubePlayer = initializedYouTubePlayer
-                initializedYouTubePlayer?.addListener(object : AbstractYouTubePlayerListener(){
-                    override fun onReady() {
-                        initializedYouTubePlayer.loadVideo("6JYIGclVQdw", 0F)
-                    }
-                })
-            }
-        }, true)
+        youtube_player_view.initialize(object: YouTubePlayerInitListener {
+            override fun onInitSuccess(initializedYouTubePlayer: YouTubePlayer?) {
+
+            youTubePlayer = initializedYouTubePlayer!!
+
+            initializedYouTubePlayer.addListener(object : AbstractYouTubePlayerListener() {
+                override fun onReady() {
+                    initializedYouTubePlayer.loadVideo("6JYIGclVQdw", 0f)
+                }
+
+            })
+        }}, true)
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_post_detail, container, false)
@@ -46,11 +55,13 @@ class PostDetail : Fragment() {
 
     }
 
+
     override fun onDestroy() {
         super.onDestroy()
 
         youtube_player_view.release()
     }
+
 
 
 }
